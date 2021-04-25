ce0dbc6eca59ed5d29513a0bd55c868c9bd48fde
corvinus-x2ze7m <rejhoka@gmail.com>
2021-04-25 22:43:07 +0200

    9.gyakorlat

--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -1,70 +1,70 @@
-﻿var kérdések;
+﻿
+var kerdesSorszam = 1;
+var helyesValasz;
 
-function letöltés() {
-    fetch('/questions.json')
-        .then(response => response.json())
-        .then(data => letöltésBefejeződött(data));
-};
-
-function letöltésBefejeződött(d) {
-    console.log("Sikeres letöltés");
-    console.log(d);
-    kérdések = d;
-    feltöltés(0);
-};
-
-window.onload = (event) => {
-    letöltés();
-
-};
-
-item = 0;
-
-function feltöltés(index) {
-    document.getElementById("kérdés_szöveg").innerHTML = kérdések[index].questionText;
-    document.getElementById("válasz1").innerHTML = kérdések[index].answer1;
-    document.getElementById("válasz2").innerHTML = kérdések[index].answer2;
-    document.getElementById("válasz3").innerHTML = kérdések[index].answer3;
-    document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kérdések[index].image;
-};
-
-
-
-function előre() {
-    visszaszinez()
-    item++;
-    if (item <= kérdések.length - 1) {
-        feltöltés(item);
-    }
-    else {
-        item = 0;
-        feltöltés(item);
-    }
-};
-
-function vissza() {
-    visszaszinez()
-    item--;
-    if (item >= 0) {
-        feltöltés(item);
-    }
-    else {
-        item = kérdések.length - 1;
-        feltöltés(item);
-    }
-};
-
-function szinezés(valaszID) {
-    if (kérdések[item].correctAnswer == parseInt(valaszID.slice(-1))) {
-        document.getElementById(valaszID).style.background = "green";
-    }
-    else {
-        document.getElementById(valaszID).style.background = "red";
-    }
-};
-
-function visszaszinez() {
-    document.getElementById("válasz1").style.background = "white";
-    document.getElementById("válasz2").style.background = "white";
-    document.getElementById("válasz3").style.background = "white";
+function kerdesBetoltes(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => kerdesMegjelenites(data));
 }
+
+function kerdesMegjelenites(kerdes) {
+    console.log(kerdes);
+    document.getElementById("kerdes_szoveg").innerText = kerdes.questionText
+    document.getElementById("valasz1").innerText = kerdes.answer1
+    document.getElementById("valasz2").innerText = kerdes.answer2
+    document.getElementById("valasz3").innerText = kerdes.answer3
+    document.getElementById("kep1").src = "https://szoft1.comeback.hu/hajo/" + kerdes.image;
+
+    //itt kezeljük le, hogy eltűntesse a blokkot, ha nincs kép, és visszategye, ha van
+    var kepDiv = document.getElementById("kep");
+    if (kerdes.image != "") {
+        if (kepDiv.style.display == "none") {
+            kepDiv.style.display = "block";
+        }
+        kep.src = "https://szoft1.comeback.hu/hajo/" + kerdes.image;
+    }
+    else {
+        kepDiv.style.display = "none";
+    }
+
+    helyesValasz = kerdes.correctAnswer;
+}
+
+
+window.onload = () => {
+    console.log("Sikeres betöltés");
+    kerdesBetoltes(kerdesSorszam);
+}
+
+var eloreLepes = document.getElementById("elore");
+eloreLepes.addEventListener("click", function () {
+
+    kerdesSorszam++;
+    kerdesBetoltes(kerdesSorszam);
+});
+
+var visszaLepes = document.getElementById("vissza");
+visszaLepes.addEventListener("click", function () {
+
+    kerdesSorszam--;
+    kerdesBetoltes(kerdesSorszam);
+});
+
+//var elsoDiv = document.getElementById("valasz1");
+//elsoDiv.addEventListener("click", function () {
+
+//    if (helyesValasz == 1) {
+//        elsoDiv.style.backgroundColor = "green";
+//    }
+//    else {
+//        elsoDiv.style.backgroundColor = "red";
+//    }
+//})
